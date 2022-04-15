4a848310b8e537c2c83eee1e205c6d2b8f459e18
Michael William Carrigan <michael.william.carrigan@cern.ch>
2022-04-15 22:09:15 +0200

    Updates to output/set triggers

--- trig3_board_trigger_waveshare_lvds-firmware/LED_4.v
+++ trig3_board_trigger_waveshare_lvds-firmware/LED_4.v
@@ -10,7 +10,15 @@ module LED_4(
 	input reg[31:0] randnum, input reg[31:0] prescale, input dorolling,
 	input [7:0] dead_time,
 	input [16-1:0] coax_in_extra, output [16-1:0] coax_out_extra, input [14-1:0] io_extra, output [28-1:0] ep4ce10_io_extra,
-	input [63:0] triggermask
+	input [63:0] triggermask,
+	input [7:0] triggernumber,
+	output reg[55:0] clockCounter[8],
+	output reg[7:0] triggerFired[8],
+	input resetClock,
+	input resetOut,
+	input triggerMask,
+	input syncClock,
+	output reg[55:0] startTimeOut
 	);
 
 reg[7:0] i;
@@ -26,35 +34,84 @@ reg [7:0] histostosend2; // to pass timing, since it's sent from the slow clk
 reg [31:0] prescale2; // to pass timing, since it's sent from the slow clk
 reg[5:0] Tout[16]; // for output triggers
 reg[2:0] Nin[64/4]; // number of groups active in each row of 4 groups
+reg[4:0] Nin_coin[8]; // number of coincidence for each channel
+reg[2:0] Nin_coin_3[8];
 reg[6:0] Nactive;//max of 16*4=64
 reg[4:0] Nactivetemp[4];//max of 4*4=16
 reg[4:0] Nactiverows;//max of 16
 reg[2:0] Nactiverowstemp[4];// max of 4
+reg[7:0] triggeruse;
+reg[7:0] lastTrigFired[8]; //when a trigger fires set this equal to the trigger number
+reg[55:0] clocksFired[8]; //array to hold the clocks fired
+reg[7:0] triggerTemp=0;
+reg resetClock2;
+reg resetOut2;
+reg isFiring=0;
+reg[2:0] triggerCounter=0; //counter for how many triggers are stored in memory
+reg trigSet[8];
+reg triggerMask2;
+reg syncClock2;
+reg[55:0] startTime=0;
+reg[2:0] hitsInRow=0;
+reg goodTrig[8];
+reg[2:0] firstTrig;
+reg firstTrigFired=0;
+reg[55:0] lastClockFired;
 
 always@(posedge clk_adc) begin
-	
+	triggeruse <= triggernumber;
 	pass_prescale <= (randnum<=prescale2);
 	resethist2<=resethist;
+	resetClock2<=resetClock;
+	resetOut2<=resetOut;
 	histostosend2<=histostosend;
 	prescale2<=prescale;
-	ext_trig_out <= (ext_trig_out_counter>0);	
+	triggerMask2<=triggerMask;
+	syncClock2<=syncClock;
+	startTimeOut<=startTime;
+	isFiring <= 0;
+	hitsInRow<=0;
+	
+	
 	i=0; while (i<64) begin
 		if (triggermask[i]) coaxinreg[i] <= ~coax_in[i]; // inputs are inverted (so that unconnected inputs are 0), then read into registers and buffered
 		else coaxinreg[i] <= 0; // masked out inputs are set to 0 regardless of input
-		if (i<8) histosout[i]<=histos[i][histostosend2]; // histo output		
+		if (i<8) begin
+		    histosout[i]<=histos[i][histostosend2]; // histo output
+		end
 		if (i<16) begin // for output stuff
 			coax_out[i] <= Tout[i]>0; // outputs fire while Tout is high
 			//coax_out[i] <= coaxinreg[i]; // passthrough		
 			if (Tout[i]>0) Tout[i] <= Tout[i]-1; // count down how long the triggers have been active
 			if (triedtofire[i]>0) triedtofire[i] <= triedtofire[i]-1; // count down deadtime for outputs
+		   if (triedtofire[i]>0) isFiring <=1; // don't fire any trigger within the coincidence time
 		end
 		i=i+1;
+	end
+	
+	if(coaxinreg[62] > 0) startTime<=counter;
+	
+	if(resetOut2 || resetClock2) begin
+		i=0; while (i<8) begin
+			lastTrigFired[i]<=0;
+			clockCounter[i]<=0;
+			triggerFired[i]<=0;
+			i=i+1;
+		end
+		triggerCounter<=0;
 	end
 	
 	// see how many "groups" (a set of two bars) are active in each "row" of 4 groups (for projective triggers)
 	// we ask for them to be >2 so that they will disappear before the calculated "vetos" will be gone
 	i=0; while (i<16) begin
-		Nin[i] <= (Tin[4*i]>2) + (Tin[4*i+1]>2) + (Tin[4*i+2]>2) + (Tin[4*i+3]>2);
+	   if (i==15) begin
+			Nin[i] <= (Tin[4*i]>2) + (Tin[4*i+1]>2); //special case to make sure Tin[15] is left for busy and Tin[14] is left for run signal mcarrigan
+			if( (Tin[4*i]>2) + (Tin[4*i+1]>2) > hitsInRow) hitsInRow <= (Tin[4*i]>2) + (Tin[4*i+1]>2);
+		end
+		else begin
+			Nin[i] <= (Tin[4*i]>2) + (Tin[4*i+1]>2) + (Tin[4*i+2]>2) + (Tin[4*i+3]>2);
+			if ((Tin[4*i]>2) + (Tin[4*i+1]>2) + (Tin[4*i+2]>2) + (Tin[4*i+3]>2) > hitsInRow) hitsInRow <= (Tin[4*i]>2) + (Tin[4*i+1]>2) + (Tin[4*i+2]>2) + (Tin[4*i+3]>2);
+		end
 		if (i<4) Nactivetemp[i] <= Nin[4*i]+Nin[4*i+1]+Nin[4*i+2]+Nin[4*i+3]; // pipelined for timing closure
 		if (i<4) Nactiverowstemp[i] <= (Nin[4*i]>0)+(Nin[4*i+1]>0)+(Nin[4*i+2]>0)+(Nin[4*i+3]>0); // pipelined for timing closure
 		i=i+1;
@@ -64,45 +121,119 @@ always@(posedge clk_adc) begin
 	//Note that it's important that we use "<=" here, since these will be updated at the _end_ of this always block and then ready to use in the _next_ clock cycle
 	//The "vetos" in each trigger below will be calculated in _this_ clock cycle and so should be present _earlier_
 	
-	// fire the outputs (0 and 1) if there are >1 input groups active
-	if (triedtofire[0]==0 && (Nactive>1) ) begin
-		if (pass_prescale) begin
-			i=0; while (i<16) begin
-				if (i==0 || i==1) Tout[i] <= 16; // fire outputs for this long
-				i=i+1;
-			end
-			triedtofire[0] <= dead_time; // will stay dead for this many clk ticks
-		end
+	
+	//Implement signal trigger - Antoine
+	i=0; while (i<8) begin // Antoine
+      Nin_coin[i] <= (Tin[i]>2)+(Tin[i+8]>2)+(Tin[i+2*8]>2)+(Tin[i+3*8]>2); // Antoine - Coincident layer if 32 first LVDS inputs are from the CAEN boards associated to the scintillator bars
+	   if (((Tin[i+3*8]==0) && (Tin[i]>2) && (Tin[i+8]>2) && (Tin[i+2*8]>2)) || ((Tin[i]==0) && (Tin[i+8]>2) && (Tin[i+2*8]>2) && (Tin[i+3*8]>2))) begin; // 3 layers coincidence
+		   Nin_coin_3[i]<=1;
+	   end
+		else begin;
+		   Nin_coin_3[i]<=0;
+      end
+		i=i+1;
 	end
 	
-	// fire the outputs (2 and 3) if there are >1 input groups active in any row
-	if (triedtofire[1]==0 && (Nin[0]>1||Nin[1]>1||Nin[2]>1||Nin[3]>1||Nin[4]>1||Nin[5]>1||Nin[6]>1||Nin[7]>1||Nin[8]>1||Nin[9]>1||Nin[10]>1||Nin[11]>1||Nin[12]>1||Nin[13]>1||Nin[14]>1||Nin[15]>1) ) begin
-		if (pass_prescale) begin
-			i=0; while (i<16) begin
-				if (i==2 || i==3) Tout[i] <= 16; // fire outputs for this long
-				i=i+1;
+	//Start Checking the 8 triggers
+	if(isFiring == 0 && coaxinreg[63] > 0) begin
+	
+		// fire the outputs if there are >1 input groups active
+		if (triggernumber[0]>0 && triedtofire[0]==0 && (Nactive>0)) begin
+			if (pass_prescale) begin
+				if(isFiring == 0) begin
+					i=0; while (i<16) begin
+						if (i<16) Tout[i] <= 16; // fire outputs for this long; changed output from 0,1 to 8 mcarrigan
+						i=i+1;
+					end
+				end
+				triedtofire[0] <= dead_time; // will stay dead for this many clk ticks
+				isFiring<=1;
+				goodTrig[0] <= 1;
+				if(goodTrig[0]==0) lastTrigFired[triggerCounter][0] <= 1'b1;
 			end
-			triedtofire[1] <= dead_time; // will stay dead for this many clk ticks
 		end
+		
+				// fire the outputs if there are >1 input groups active
+		if (triggernumber[1]>0 && triedtofire[1]==0 && (Nactive>1) ) begin
+			if (pass_prescale) begin
+				if(isFiring == 0) begin
+					i=0; while (i<16) begin
+						if (i<16) Tout[i] <= 16; // fire outputs for this long; changed output from 0,1 to 8 mcarrigan
+						i=i+1;
+					end
+				end
+				triedtofire[1] <= dead_time; // will stay dead for this many clk ticks
+				isFiring<=1;
+				goodTrig[1] <= 1;
+				if(goodTrig[1]==0) lastTrigFired[triggerCounter][1] <= 1'b1;
+			end
+		end
+		
+		if (triggernumber[2]>0 && triedtofire[2]==0 && (Nactive>2) ) begin
+			if (pass_prescale) begin
+				if(isFiring == 0) begin
+					i=0; while (i<16) begin
+						if (i<16) Tout[i] <= 16; // fire outputs for this long; changed output from 0,1 to 8 mcarrigan
+						i=i+1;
+					end
+				end
+				triedtofire[2] <= dead_time; // will stay dead for this many clk ticks
+				isFiring<=1;
+				goodTrig[2] <= 1;
+				if(goodTrig[2]==0) lastTrigFired[triggerCounter][2] <= 1'b1;
+			end
+		end
+		
+		if (triggernumber[3]>0 && triedtofire[3]==0 && (Nactive>3) ) begin
+			if (pass_prescale) begin
+				if(isFiring == 0) begin
+					i=0; while (i<16) begin
+						if (i<16) Tout[i] <= 16; // fire outputs for this long; changed output from 0,1 to 8 mcarrigan
+						i=i+1;
+					end
+				end
+				triedtofire[3] <= dead_time; // will stay dead for this many clk ticks
+				isFiring<=1;
+				goodTrig[3] <= 1;
+				if(goodTrig[3]==0) lastTrigFired[triggerCounter][3] <= 1'b1;
+			end
+		end
+		
 	end
 	
-	// fire the outputs (4 and 5) if there are >2 input groups active in any row
-	if (triedtofire[2]==0 && (Nin[0]>2||Nin[1]>2||Nin[2]>2||Nin[3]>2||Nin[4]>2||Nin[5]>2||Nin[6]>2||Nin[7]>2||Nin[8]>2||Nin[9]>2||Nin[10]>2||Nin[11]>2||Nin[12]>2||Nin[13]>2||Nin[14]>2||Nin[15]>2) ) begin
+		// fire the outputs if there are >1 input groups active in any row
+		/*if (triggernumber[1]>0 && triedtofire[1]==0 && (Nin[0]>3||Nin[1]>3||Nin[2]>3||Nin[3]>3||Nin[4]>3||Nin[5]>3||Nin[6]>3||Nin[7]>3||Nin[8]>3||Nin[9]>3||Nin[10]>3||Nin[11]>3||Nin[12]>3||Nin[13]>3||Nin[14]>3||Nin[15]>3) ) begin
+			if (pass_prescale) begin
+				if(isFiring==0) begin
+					i=0; while (i<16) begin
+						if (i<16) Tout[i] <= 16; // fire outputs for this long; changed output from 2,3 to 8 mcarrigan
+						i=i+1;
+					end
+				end
+				triedtofire[1] <= dead_time; // will stay dead for this many clk ticks
+				isFiring<=1;
+			end
+		end
+	end*/
+	
+	// fire the outputs if there are >2 input groups active in any row
+	/*if (triggernumber[2]>0 && triedtofire[2]==0 && (Nin[0]>2||Nin[1]>2||Nin[2]>2||Nin[3]>2||Nin[4]>2||Nin[5]>2||Nin[6]>2||Nin[7]>2||Nin[8]>2||Nin[9]>2||Nin[10]>2||Nin[11]>2||Nin[12]>2||Nin[13]>2||Nin[14]>2||Nin[15]>2) ) begin
 		if (pass_prescale) begin
 			i=0; while (i<16) begin
-				if (i==4 || i==5) Tout[i] <= 16; // fire outputs for this long
+				//if (i==4 || i==5) Tout[i] <= 16; // fire outputs for this long
+				if(i<16) Tout[i] <= 16;
 				i=i+1;
 			end
 			triedtofire[2] <= dead_time; // will stay dead for this many clk ticks
 		end
 	end
 	
-	// fire the outputs (6 and 7) if there are >2 input groups active in any row, and just 1 row with any input groups active
-	if (triedtofire[3]==0 && (Nin[0]>2||Nin[1]>2||Nin[2]>2||Nin[3]>2||Nin[4]>2||Nin[5]>2||Nin[6]>2||Nin[7]>2||Nin[8]>2||Nin[9]>2||Nin[10]>2||Nin[11]>2||Nin[12]>2||Nin[13]>2||Nin[14]>2||Nin[15]>2) 
+	// fire the outputs if there are >2 input groups active in any row, and just 1 row with any input groups active
+	if (triggernumber[3]>0 && triedtofire[3]==0 && (Nin[0]>2||Nin[1]>2||Nin[2]>2||Nin[3]>2||Nin[4]>2||Nin[5]>2||Nin[6]>2||Nin[7]>2||Nin[8]>2||Nin[9]>2||Nin[10]>2||Nin[11]>2||Nin[12]>2||Nin[13]>2||Nin[14]>2||Nin[15]>2) 
 								 && (Nactiverows<2) ) begin
 		if (pass_prescale) begin
 			i=0; while (i<16) begin
-				if (i==6 || i==7) Tout[i] <= 16; // fire outputs for this long
+				if (i<16) Tout[i] <= 16; // fire outputs for this long
 				i=i+1;
 			end
 			triedtofire[3] <= dead_time; // will stay dead for this many clk ticks
@@ -110,16 +241,76 @@ always@(posedge clk_adc) begin
 	end
 	
 	// fire the output (8) if there are >0 input groups active (good for testing inputs)
-	if (triedtofire[4]==0 && (Nactive>0) ) begin
+	// added busy veto Tin[15] mcarrigan
+	// replace Tin[15] by coaxinreg for more acccurate veto + add trigger number //Antoine
+	if ( triggernumber[4]>0 && triedtofire[4]==0 && (Nactive>2) ) begin
 		if (pass_prescale) begin
 			i=0; while (i<16) begin
-				if (i==8) Tout[i] <= 16; // fire outputs for this long
+				if (i<16) Tout[i] <= 16; // fire outputs for this long, output to 4 coax outputs
 				i=i+1;
 			end
 			triedtofire[4] <= dead_time; // will stay dead for this many clk ticks
-			led[1] <= 1'b0; // turn on the LED
+			//led[1] <= 1'b0; // turn on the LED
+		end
+	end	
+	if ( triggernumber[5]>0 && triedtofire[5]==0 && (Nactivetemp[0]>1) ) begin
+		if (pass_prescale) begin
+			i=0; while (i<16) begin
+				if (i<16) Tout[i] <= 16; // fire outputs for this long, output to 4 coax outputs
+				i=i+1;
+			end
+			triedtofire[5] <= dead_time; // will stay dead for this many clk ticks
+			//led[1] <= 1'b0; // turn on the LED
 		end
 	end
+	
+	//Implementing coincidence triggers - Antoine
+	if ( triggernumber[6]>0 && triedtofire[6]==0 && (Nin_coin[0]>3||Nin_coin[1]>3||Nin_coin[2]>3||Nin_coin[3]>3||Nin_coin[4]>3||Nin_coin[5]>3||Nin_coin[6]>3||Nin_coin[7]>3)) begin // Antoine - 4 layers coincidence 
+		if (pass_prescale) begin
+			i=0; while (i<16) begin
+				if (i<16) Tout[i] <= 16; // fire outputs for this long, output to 4 coax outputs
+				i=i+1;
+			end
+			triedtofire[6] <= dead_time; // will stay dead for this many clk ticks
+			//led[1] <= 1'b0; // turn on the LED
+		end
+	end	
+
+	if ( triggernumber[7]>0 && triedtofire[7]==0 && (Nin_coin_3[0]>0||Nin_coin_3[1]>0||Nin_coin_3[2]>0||Nin_coin_3[3]>0||Nin_coin_3[4]>0||Nin_coin_3[5]>0||Nin_coin_3[6]>0||Nin_coin_3[7]>0)) begin // Antoine - 3 layers coincidence 
+		if (pass_prescale) begin
+			i=0; while (i<16) begin
+				if (i<16) Tout[i] <= 16; // fire outputs for this long, output to 4 coax outputs
+				i=i+1;
+			end
+			triedtofire[7] <= dead_time; // will stay dead for this many clk ticks
+			//led[1] <= 1'b0; // turn on the LED
+		end
+	end
+	end*/
+
+	/*if ( triggernumber[6]>0 && triedtofire[9]==0 && isFiring==0 && coaxinreg[15] > 0) begin // mcarrigan, check clock in 
+		//if (pass_prescale) begin
+		//clockCounter<= clockCounter + 1;
+		i=0; while (i<16) begin
+			if (i>3) Tout[i] <= 1; // fire outputs for this long, output to 4 coax outputs
+			i=i+1;
+		end
+		//lastTrigFired <= 6;
+		triedtofire[9] <= dead_time; // will stay dead for this many clk ticks
+		led[1] <= 1'b0; // turn on the LED
+	end*/
+	
+	//testing trigger number //Antoine
+	/*if (triggernumber[1]>0 && triedtofire[6]==0 && isFiring==0 && coaxinreg[15] > 0 && (Nactive>3) ) begin
+		if (pass_prescale) begin
+			i=0; while (i<16) begin
+				if (i<16) Tout[i] <= 16; // fire outputs for this long, output to 4 coax outputs
+				i=i+1;
+			end
+			triedtofire[6] <= dead_time; // will stay dead for this many clk ticks
+			//led[1] <= 1'b0; // turn on the LED
+		end		
+	end*/
 	
 	//rolling trigger (about 119.21 Hz)
 	if (autocounter[20]) begin
@@ -132,12 +323,36 @@ always@(posedge clk_adc) begin
 	end
 	
 	if (led[0]==1'b1) led[1]<=1'b1; // turn it off when the other led toggles, so we can see it turn back on
-	
+
+   i=0; while (i<8) begin	
+		if (triedtofire[i]>0 && trigSet[i]==0 && triggerMask2==0) begin
+			//lastTrigFired[triggerCounter][i] <= 1'b1;
+			trigSet[i]<=1;
+		end
+		if (triedtofire[i]==0) trigSet[i]<=0; //reset to allow triggerFired to output this trigger again
+		if(firstTrigFired==0) begin
+			firstTrig<=i;
+			firstTrigFired<=1;
+			lastClockFired<=counter;
+		end
+		i=i+1;
+	end
+		
+	if(lastTrigFired[triggerCounter]>0 && !syncClock2 && firstTrigFired==1 && triedtofire[firstTrig]==0) begin
+	   triggerFired[triggerCounter] <= lastTrigFired[triggerCounter];
+		clockCounter[triggerCounter] <= lastClockFired;
+		triggerCounter<=triggerCounter+1;
+		firstTrigFired<=0;
+		i=0; while (i<8) begin
+			goodTrig[i]<=0;
+			i=i+1;
+		end
+   end
 end
 
 // triggers (from other boards) are read in and monitored
 reg[5:0] Tin[64];
-always @(posedge clk_adc) begin		
+always @(posedge clk_adc) begin	
 	j=0; while (j<64) begin
 		
 		// buffer inputs
@@ -164,12 +379,18 @@ end
 
 
 //for LEDs
-reg[31:0] counter=0;
+reg[51:0] counter=0;
 always@(posedge clk) begin
-	counter<=counter+1;
+	if (ext_trig_out) begin
+		if(!resetClock2) counter<=counter+1;
+		if(resetClock2) counter<=0;
+	end
 	led[0]<=counter[26]; // flashing
 	led[2]<=dorolling;
 	led[3]<=clk_locked;
+	ext_trig_out <= !ext_trig_out;
+
 end
+
 	
 endmodule
--- trig3_board_trigger_waveshare_lvds-firmware/pll2.v
+++ trig3_board_trigger_waveshare_lvds-firmware/pll2.v
@@ -48,6 +48,7 @@ module pll2 (
 	activeclock,
 	c0,
 	c1,
+	c2,
 	locked,
 	phasedone);
 
@@ -62,6 +63,7 @@ module pll2 (
 	output	  activeclock;
 	output	  c0;
 	output	  c1;
+	output	  c2;
 	output	  locked;
 	output	  phasedone;
 `ifndef ALTERA_RESERVED_QIS
@@ -76,33 +78,35 @@ module pll2 (
 // synopsys translate_on
 `endif
 
-	wire  sub_wire3;
-	wire [4:0] sub_wire4;
-	wire  sub_wire7;
-	wire  sub_wire8;
-	wire  sub_wire2 = inclk1;
-	wire  sub_wire0 = inclk0;
-	wire [1:0] sub_wire1 = {sub_wire2, sub_wire0};
-	wire  activeclock = sub_wire3;
-	wire [1:1] sub_wire6 = sub_wire4[1:1];
-	wire [0:0] sub_wire5 = sub_wire4[0:0];
-	wire  c0 = sub_wire5;
-	wire  c1 = sub_wire6;
-	wire  locked = sub_wire7;
-	wire  phasedone = sub_wire8;
+	wire  sub_wire0;
+	wire [4:0] sub_wire1;
+	wire  sub_wire5;
+	wire  sub_wire6;
+	wire  sub_wire9 = inclk1;
+	wire  activeclock = sub_wire0;
+	wire [2:2] sub_wire4 = sub_wire1[2:2];
+	wire [1:1] sub_wire3 = sub_wire1[1:1];
+	wire [0:0] sub_wire2 = sub_wire1[0:0];
+	wire  c0 = sub_wire2;
+	wire  c1 = sub_wire3;
+	wire  c2 = sub_wire4;
+	wire  locked = sub_wire5;
+	wire  phasedone = sub_wire6;
+	wire  sub_wire7 = inclk0;
+	wire [1:0] sub_wire8 = {sub_wire9, sub_wire7};
 
 	altpll	altpll_component (
 				.areset (areset),
 				.clkswitch (clkswitch),
-				.inclk (sub_wire1),
+				.inclk (sub_wire8),
 				.phasecounterselect (phasecounterselect),
 				.phasestep (phasestep),
 				.phaseupdown (phaseupdown),
 				.scanclk (scanclk),
-				.activeclock (sub_wire3),
-				.clk (sub_wire4),
-				.locked (sub_wire7),
-				.phasedone (sub_wire8),
+				.activeclock (sub_wire0),
+				.clk (sub_wire1),
+				.locked (sub_wire5),
+				.phasedone (sub_wire6),
 				.clkbad (),
 				.clkena ({6{1'b1}}),
 				.clkloss (),
@@ -139,6 +143,10 @@ module pll2 (
 		altpll_component.clk1_duty_cycle = 50,
 		altpll_component.clk1_multiply_by = 5,
 		altpll_component.clk1_phase_shift = "0",
+		altpll_component.clk2_divide_by = 1,
+		altpll_component.clk2_duty_cycle = 50,
+		altpll_component.clk2_multiply_by = 2,
+		altpll_component.clk2_phase_shift = "0",
 		altpll_component.compensate_clock = "CLK0",
 		altpll_component.inclk0_input_frequency = 20000,
 		altpll_component.inclk1_input_frequency = 20000,
@@ -174,7 +182,7 @@ module pll2 (
 		altpll_component.port_scanwrite = "PORT_UNUSED",
 		altpll_component.port_clk0 = "PORT_USED",
 		altpll_component.port_clk1 = "PORT_USED",
-		altpll_component.port_clk2 = "PORT_UNUSED",
+		altpll_component.port_clk2 = "PORT_USED",
 		altpll_component.port_clk3 = "PORT_UNUSED",
 		altpll_component.port_clk4 = "PORT_UNUSED",
 		altpll_component.port_clk5 = "PORT_UNUSED",
@@ -220,10 +228,13 @@ endmodule
 // Retrieval info: PRIVATE: DEVICE_SPEED_GRADE STRING "6"
 // Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "1"
 // Retrieval info: PRIVATE: DIV_FACTOR1 NUMERIC "2"
+// Retrieval info: PRIVATE: DIV_FACTOR2 NUMERIC "1"
 // Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
 // Retrieval info: PRIVATE: DUTY_CYCLE1 STRING "50.00000000"
+// Retrieval info: PRIVATE: DUTY_CYCLE2 STRING "50.00000000"
 // Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "50.000000"
 // Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE1 STRING "125.000000"
+// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE2 STRING "100.000000"
 // Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
 // Retrieval info: PRIVATE: EXT_FEEDBACK_RADIO STRING "0"
 // Retrieval info: PRIVATE: GLOCKED_COUNTER_EDIT_CHANGED STRING "1"
@@ -245,27 +256,35 @@ endmodule
 // Retrieval info: PRIVATE: LVDS_MODE_DATA_RATE_DIRTY NUMERIC "0"
 // Retrieval info: PRIVATE: LVDS_PHASE_SHIFT_UNIT0 STRING "deg"
 // Retrieval info: PRIVATE: LVDS_PHASE_SHIFT_UNIT1 STRING "ps"
+// Retrieval info: PRIVATE: LVDS_PHASE_SHIFT_UNIT2 STRING "ps"
 // Retrieval info: PRIVATE: MANUAL_PHASE_SHIFT_STEP_EDIT STRING "1.00000000"
 // Retrieval info: PRIVATE: MANUAL_PHASE_SHIFT_STEP_UNIT STRING "ps"
 // Retrieval info: PRIVATE: MIG_DEVICE_SPEED_GRADE STRING "Any"
 // Retrieval info: PRIVATE: MIRROR_CLK0 STRING "0"
 // Retrieval info: PRIVATE: MIRROR_CLK1 STRING "0"
+// Retrieval info: PRIVATE: MIRROR_CLK2 STRING "0"
 // Retrieval info: PRIVATE: MULT_FACTOR0 NUMERIC "1"
 // Retrieval info: PRIVATE: MULT_FACTOR1 NUMERIC "5"
+// Retrieval info: PRIVATE: MULT_FACTOR2 NUMERIC "1"
 // Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "216.00000000"
 // Retrieval info: PRIVATE: OUTPUT_FREQ1 STRING "100.00000000"
+// Retrieval info: PRIVATE: OUTPUT_FREQ2 STRING "100.00000000"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "0"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_MODE1 STRING "0"
+// Retrieval info: PRIVATE: OUTPUT_FREQ_MODE2 STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT0 STRING "MHz"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT1 STRING "MHz"
+// Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT2 STRING "MHz"
 // Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "1"
 // Retrieval info: PRIVATE: PHASE_RECONFIG_INPUTS_CHECK STRING "1"
 // Retrieval info: PRIVATE: PHASE_SHIFT0 STRING "0.00000000"
 // Retrieval info: PRIVATE: PHASE_SHIFT1 STRING "0.00000000"
+// Retrieval info: PRIVATE: PHASE_SHIFT2 STRING "0.00000000"
 // Retrieval info: PRIVATE: PHASE_SHIFT_STEP_ENABLED_CHECK STRING "1"
 // Retrieval info: PRIVATE: PHASE_SHIFT_UNIT0 STRING "deg"
 // Retrieval info: PRIVATE: PHASE_SHIFT_UNIT1 STRING "ps"
+// Retrieval info: PRIVATE: PHASE_SHIFT_UNIT2 STRING "ps"
 // Retrieval info: PRIVATE: PLL_ADVANCED_PARAM_CHECK STRING "0"
 // Retrieval info: PRIVATE: PLL_ARESET_CHECK STRING "1"
 // Retrieval info: PRIVATE: PLL_AUTOPLL_CHECK NUMERIC "1"
@@ -289,13 +308,16 @@ endmodule
 // Retrieval info: PRIVATE: SRC_SYNCH_COMP_RADIO STRING "0"
 // Retrieval info: PRIVATE: STICKY_CLK0 STRING "1"
 // Retrieval info: PRIVATE: STICKY_CLK1 STRING "1"
+// Retrieval info: PRIVATE: STICKY_CLK2 STRING "1"
 // Retrieval info: PRIVATE: SWITCHOVER_COUNT_EDIT NUMERIC "1"
 // Retrieval info: PRIVATE: SWITCHOVER_FEATURE_ENABLED STRING "1"
 // Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 // Retrieval info: PRIVATE: USE_CLK0 STRING "1"
 // Retrieval info: PRIVATE: USE_CLK1 STRING "1"
+// Retrieval info: PRIVATE: USE_CLK2 STRING "1"
 // Retrieval info: PRIVATE: USE_CLKENA0 STRING "0"
 // Retrieval info: PRIVATE: USE_CLKENA1 STRING "0"
+// Retrieval info: PRIVATE: USE_CLKENA2 STRING "0"
 // Retrieval info: PRIVATE: USE_MIL_SPEED_GRADE NUMERIC "0"
 // Retrieval info: PRIVATE: ZERO_DELAY_RADIO STRING "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
@@ -308,6 +330,10 @@ endmodule
 // Retrieval info: CONSTANT: CLK1_DUTY_CYCLE NUMERIC "50"
 // Retrieval info: CONSTANT: CLK1_MULTIPLY_BY NUMERIC "5"
 // Retrieval info: CONSTANT: CLK1_PHASE_SHIFT STRING "0"
+// Retrieval info: CONSTANT: CLK2_DIVIDE_BY NUMERIC "1"
+// Retrieval info: CONSTANT: CLK2_DUTY_CYCLE NUMERIC "50"
+// Retrieval info: CONSTANT: CLK2_MULTIPLY_BY NUMERIC "2"
+// Retrieval info: CONSTANT: CLK2_PHASE_SHIFT STRING "0"
 // Retrieval info: CONSTANT: COMPENSATE_CLOCK STRING "CLK0"
 // Retrieval info: CONSTANT: INCLK0_INPUT_FREQUENCY NUMERIC "20000"
 // Retrieval info: CONSTANT: INCLK1_INPUT_FREQUENCY NUMERIC "20000"
@@ -342,7 +368,7 @@ endmodule
 // Retrieval info: CONSTANT: PORT_SCANWRITE STRING "PORT_UNUSED"
 // Retrieval info: CONSTANT: PORT_clk0 STRING "PORT_USED"
 // Retrieval info: CONSTANT: PORT_clk1 STRING "PORT_USED"
-// Retrieval info: CONSTANT: PORT_clk2 STRING "PORT_UNUSED"
+// Retrieval info: CONSTANT: PORT_clk2 STRING "PORT_USED"
 // Retrieval info: CONSTANT: PORT_clk3 STRING "PORT_UNUSED"
 // Retrieval info: CONSTANT: PORT_clk4 STRING "PORT_UNUSED"
 // Retrieval info: CONSTANT: PORT_clk5 STRING "PORT_UNUSED"
@@ -368,6 +394,7 @@ endmodule
 // Retrieval info: USED_PORT: areset 0 0 0 0 INPUT GND "areset"
 // Retrieval info: USED_PORT: c0 0 0 0 0 OUTPUT_CLK_EXT VCC "c0"
 // Retrieval info: USED_PORT: c1 0 0 0 0 OUTPUT_CLK_EXT VCC "c1"
+// Retrieval info: USED_PORT: c2 0 0 0 0 OUTPUT_CLK_EXT VCC "c2"
 // Retrieval info: USED_PORT: clkswitch 0 0 0 0 INPUT GND "clkswitch"
 // Retrieval info: USED_PORT: inclk0 0 0 0 0 INPUT_CLK_EXT GND "inclk0"
 // Retrieval info: USED_PORT: inclk1 0 0 0 0 INPUT_CLK_EXT GND "inclk1"
@@ -388,6 +415,7 @@ endmodule
 // Retrieval info: CONNECT: activeclock 0 0 0 0 @activeclock 0 0 0 0
 // Retrieval info: CONNECT: c0 0 0 0 0 @clk 0 0 1 0
 // Retrieval info: CONNECT: c1 0 0 0 0 @clk 0 0 1 1
+// Retrieval info: CONNECT: c2 0 0 0 0 @clk 0 0 1 2
 // Retrieval info: CONNECT: locked 0 0 0 0 @locked 0 0 0 0
 // Retrieval info: CONNECT: phasedone 0 0 0 0 @phasedone 0 0 0 0
 // Retrieval info: GEN_FILE: TYPE_NORMAL pll2.v TRUE
